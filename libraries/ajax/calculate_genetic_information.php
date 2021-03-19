c5ec6b88a66544e19b2fe850c9e9c8c6843e77a3
cvwill90 <christophe.villamizar@gmail.com>
2021-03-19 09:10:33 +0100

    Add Backend script for fetching genetic information

---
+++ libraries/ajax/calculate_genetic_information.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$animal_genetic_information = [
+    "livre_genealogique" => "Hello ",
+    "pourcentage_sang_race" => 123
+];
+
+return json_encode($animal_genetic_information);
